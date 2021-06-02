997c4a45a18beec2d0f97120b8faf8fbcbf40120
socrat89 <socratalnajjad89@gmail.com>
2021-06-03 00:05:06 +0300

    Creating Table

--- answers.sh
+++ answers.sh
@@ -5,3 +5,23 @@ SELECT Points FROM students WHERE Name="Alex";
 INSERT INTO students (name,Age,Gender,Points) VALUES ("Socrat",32,"M",600);
 UPDATE students SET points=(points+100) WHERE name="Basma";
 UPDATE students SET points=(points-1) WHERE name="Alex";
+
+
+# ///////////// Creating Table
+CREATE TABLE "graduates" (
+"ID" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+"Name" TEXT NOT Null UNIQUE,
+"Age" INTEGER,
+"Gender" TEXT,
+"Points" INTEGER,
+"Graduation" TEXT 
+);
+# /////////////// Copy Layal's data from students to graduates
+INSERT INTO  graduates (Id,Name,Age,Gender,Points)
+SELECT Id,Name,Age,Gender,Points FROM students WHERE name="Layal";
+UPDATE graduates SET Graduation = '08/09/2018' WHERE name="Layal";#/// ADD graduation date
+DELETE FROM students WHERE name = "Layal"; #// Remove Layal's record from students
+SELECT * FROM graduates;#// checking 
+SELECT * FROM students;#// checking 
+
+
