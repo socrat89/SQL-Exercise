7150a8e05809355653404375fbbf5823e42a8fb4
socrat89 <socratalnajjad89@gmail.com>
2021-06-04 00:19:42 +0300

    Basic Queries

--- answers.sh
+++ answers.sh
@@ -1,6 +1,7 @@
 SELECT Name FROM students;
 SELECT * FROM students WHERE Age > 30;
-SELECT Name FROM students WHERE Age > 30 AND Gender = "F";
+SELECT Name FROM students WHERE Age > 30 AND Gender = "F";# femal whose above 30 the result is jana
+SELECT Name FROM students WHERE Age =30 AND Gender = "F";# femal whose age is 30 the Result Non
 SELECT Points FROM students WHERE Name="Alex";
 INSERT INTO students (name,Age,Gender,Points) VALUES ("Socrat",32,"M",600);
 UPDATE students SET points=(points+100) WHERE name="Basma";
